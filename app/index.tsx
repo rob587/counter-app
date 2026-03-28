6389a2f72ea745708d4a20b722b3a00ee030ab0a
rob587 <robertocamm02@yahoo.com>
2026-03-28 12:44:01 +0100

    added state to manage counting and button with values to count positive, negative and reset

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,29 @@
-import { Text, View } from "react-native";
-
+import { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 export default function Index() {
+  const [counter, setCounter] = useState(0);
+
+  const increment = () => setCounter(counter + 1);
+  const resetCount = () => setCounter(0);
+  const decrement = () => setCounter(counter - 1);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.contenitore}>
+      <Text>Counting </Text>
+
+      <Button onPress={increment} title="Aumenta" />
+      <Button onPress={resetCount} title="Reset" />
+      <Button onPress={decrement} title="Diminuisci" />
+
+      <Text> conteggio: {counter}</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  contenitore: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
