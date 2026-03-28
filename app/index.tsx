62df4a2ec95acadc45e1010abadf8eea5ef29c83
rob587 <robertocamm02@yahoo.com>
2026-03-28 13:07:18 +0100

    Added conditions to btns based in positives and negatives

--- app/index.tsx
+++ app/index.tsx
@@ -2,24 +2,34 @@ import { useState } from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
 export default function Index() {
   const [counter, setCounter] = useState(0);
+  const [active, setActive] = useState("verde");
 
   const increment = () => setCounter(counter + 1);
   const resetCount = () => setCounter(0);
   const decrement = () => setCounter(counter - 1);
 
+  const coloreTesto = counter < 0 ? "red" : counter > 0 ? "green" : "black";
+
   return (
     <View style={styles.contenitore}>
       <Text style={styles.mainTitle}>Counting </Text>
-      <View style={styles.bottoni}>
-        <Button onPress={increment} title="Aumenta" />
+      <View style={[styles.bottoni]}>
+        <Button onPress={increment} title="Aumenta" disabled={counter >= 10} />
       </View>
       <View style={styles.bottoni}>
         <Button onPress={resetCount} title="Reset" />
       </View>
       <View style={styles.bottoni}>
-        <Button onPress={decrement} title="Diminuisci" />
+        <Button
+          onPress={decrement}
+          title="Diminuisci"
+          disabled={counter <= -10}
+        />
       </View>
-      <Text> conteggio: {counter}</Text>
+      <Text style={[styles.empty, { color: coloreTesto }]}>
+        {" "}
+        conteggio: {counter}
+      </Text>
     </View>
   );
 }
@@ -40,4 +50,6 @@ const styles = StyleSheet.create({
     fontSize: 30,
     marginBottom: 15,
   },
+
+  empty: {},
 });
